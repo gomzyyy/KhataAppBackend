0f3d8695d32c30bd658cd847ee0b6c812ef4ccee
gomzyyy <158289133+gomzyyy@users.noreply.github.com>
2025-04-01 23:23:09 +0530

    added new property 'createdBy' in Product schema for more clarity about the creator.

--- Backend-KhataApp/models/Product/Product.js
+++ Backend-KhataApp/models/Product/Product.js
@@ -1,8 +1,21 @@
 import { Schema, model } from "mongoose";
 import { sharedProduct } from "../Shared/shared.product.js";
+import { CreatedByModel } from "../../constants/enums.js";
 
 const productSchema = new Schema(
-  {...sharedProduct},
+  {
+    ...sharedProduct,
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      refPath: "createdByModel",
+      required: true,
+    },
+    createdByModel: {
+      type: String,
+      required: true,
+      enum: [...CreatedByModel],
+    },
+  },
   { timestamps: true }
 );
 const Product = model("Product", productSchema);
