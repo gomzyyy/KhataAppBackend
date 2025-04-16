401fb85364dc77e12e450905dbce78bc24d1cb9a
gomzyyy <158289133+gomzyyy@users.noreply.github.com>
2025-04-17 00:09:26 +0530

    added sessions to make everything well synchronised in customer creation

--- Backend-KhataApp/controllers/customer/c/customer.create.js
+++ Backend-KhataApp/controllers/customer/c/customer.create.js
@@ -5,34 +5,52 @@ import mongoose from "mongoose";
 import { uploadToCloudinary } from "../../../service/cloud.js";
 
 export const createCustomerController = async (req, res) => {
+  const session = await mongoose.startSession();
+  session.startTransaction();
   try {
     const { creatorId, createdBy } = req.query;
     const { name, address, businessOwnerId, phoneNumber, email } = req.body;
 
     if (!name || !businessOwnerId) {
+      await session.abortTransaction();
       return res.status(resType.BAD_REQUEST.code).json({
         message: "Some required fields are missing",
         success: false,
       });
     }
     if (!creatorId || !createdBy || !CreatedByModel.includes(createdBy)) {
+      await session.abortTransaction();
       return res.status(resType.BAD_REQUEST.code).json({
-        message: "You are not authorised of this action.",
+        message: "You are not authorized to perform this action.",
         success: false,
       });
     }
 
     let existingCustomer;
     if (phoneNumber) {
-      existingCustomer = await Customer.findOne({ phoneNumber });
+      existingCustomer = await Customer.findOne({ phoneNumber }).session(
+        session
+      );
     } else {
-      existingCustomer = await Customer.findOne({ name });
+      existingCustomer = await Customer.findOne({ name }).session(session);
     }
     if (existingCustomer) {
+      await session.abortTransaction();
       return res.status(resType.CONFLICT.code).json({
         message: "Customer already exists with the given credentials",
         success: false,
       });
+    }
+
+    if (email) {
+      const existingEmail = await Customer.findOne({ email }).session(session);
+      if (existingEmail) {
+        await session.abortTransaction();
+        return res.status(resType.CONFLICT.code).json({
+          message: "Customer already exists with the given email",
+          success: false,
+        });
+      }
     }
 
     let creator;
@@ -41,16 +59,21 @@ export const createCustomerController = async (req, res) => {
         creator = await Partner.findOne({
           _id: creatorId,
           "permissions.customer.create": true,
-        })?.select("-password");
+        })
+          .select("-password")
+          .session(session);
       } else if (createdBy === "Employee") {
         creator = await Employee.findOne({
           _id: creatorId,
           "permissions.customer.create": true,
-        })?.select("-password");
+        })
+          .select("-password")
+          .session(session);
       } else {
-        creator = await Owner.findById(creatorId);
+        creator = await Owner.findById(creatorId).session(session);
       }
     } else {
+      await session.abortTransaction();
       return res.status(resType.BAD_REQUEST.code).json({
         message: "Invalid creator Object ID.",
         success: false,
@@ -58,6 +81,7 @@ export const createCustomerController = async (req, res) => {
     }
 
     if (!creator) {
+      await session.abortTransaction();
       return res.status(resType.NOT_FOUND.code).json({
         message: "You are not an Authorised user to perform this action",
         success: false,
@@ -66,16 +90,18 @@ export const createCustomerController = async (req, res) => {
 
     let foundBusinessOwner;
     if (mongoose.Types.ObjectId.isValid(businessOwnerId)) {
-      foundBusinessOwner = await Owner.findById(businessOwnerId).select(
-        "-password"
-      );
+      foundBusinessOwner = await Owner.findById(businessOwnerId)
+        .select("-password")
+        .session(session);
       if (!foundBusinessOwner) {
+        await session.abortTransaction();
         return res.status(resType.NOT_FOUND.code).json({
           message: "No owner found or Incorrect Owner ID",
           success: false,
         });
       }
     } else {
+      await session.abortTransaction();
       return res.status(resType.BAD_REQUEST.code).json({
         message: "Invalid Owner Object ID.",
         success: false,
@@ -106,7 +132,11 @@ export const createCustomerController = async (req, res) => {
     };
     const newCustomer = new Customer(newCustomerObj);
     foundBusinessOwner.customers.push(newCustomer._id);
-    await Promise.all([newCustomer.save(), foundBusinessOwner.save()]);
+    await Promise.all([
+      newCustomer.save({ session }),
+      foundBusinessOwner.save({ session }),
+    ]);
+    session.commitTransaction();
     return res.status(resType.OK.code).json({
       message: "Customer created successfully!",
       data: {
@@ -123,5 +153,7 @@ export const createCustomerController = async (req, res) => {
       }`,
       success: false,
     });
+  } finally {
+    session.endSession();
   }
 };
